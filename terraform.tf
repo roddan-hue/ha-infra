db11e99489cc6455baaee240e2a5602931485d12
roddaniels <rodbagares.au@gmail.com>
2026-09-18 16:43:16 +1000

    readme updates, iam policy, configure OIDC for github, configure terraform workspace/project

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,18 @@
 terraform {
-    cloud{
+    cloud {
+        organization = "bossrod"
 
         workspaces {
             project = "HA Terraform"
-            name = "ha-aws"
+            name    = "ha-aws"
         }
     }
 
+    required_version = ">= 1.2"
     required_providers {
         aws = {
             source  = "hashicorp/aws"
             version = "~> 6.57"
         }
     }
-    required_version = ">= 1.2"
 }
