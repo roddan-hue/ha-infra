6dc1e0805139743b61bc54110585e48065bd11a9
roddaniels <rodbagares.au@gmail.com>
2026-09-21 11:01:23 +1000

    update policy issue, challenging to determine each policy. And update instance type free tier to t3.micro as t2.micro is not supported in region

--- main.tf
+++ main.tf
@@ -160,7 +160,7 @@ resource "aws_autoscaling_policy" "cpu" {
 resource "aws_launch_template" "ha_infra" {
   name_prefix   = "ha-infra-"
   image_id      = data.aws_ami.latest_amazon_linux.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [
     aws_security_group.instance.id,
   ]
